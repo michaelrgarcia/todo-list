115175623414ba6e06fa3b2604e08b67f975259f
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-12-10 11:51:45 -0800

    Add default project + everything render function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { header, main, dialog } from "./siteBuilder.js";
+import { displayDefault, updateScreen } from "./projectTaskRender.js";
 
 const content = document.getElementById("content");
-
 content.append(header(), main(), dialog());
+
+displayDefault();
 
 window.addEventListener("click", function(event) {
     if (event.target.id == "add-task") {
         document.querySelector("dialog").showModal();
     }
-});
+}); 
+
+updateScreen();
