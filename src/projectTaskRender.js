84dea1683b978a8ca0388f47a5796b8f42e28148
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-12-10 11:34:57 -0800

    Initial create + progress on rendering in DOM

---
+++ src/projectTaskRender.js
@@ -0,0 +1,45 @@
+import { createProject, createTask } from "./projectTaskCreator";
+import elementCrafter from "./craftingTable";
+
+const projects = [];
+const domProjectName = document.querySelector(".project-name");
+
+const test = createProject("hello", true);
+const testTask = createTask("imtest", "hello", "im a test task", "12/23/23", false);
+test.tasks.push(testTask);
+projects.push(test);
+
+function updateProjects() {
+    const projectsDomMenu = document.getElementById("projects-menu");
+
+}
+
+function updateTasks() {
+    const tasks = document.getElementById("tasks");
+    if (tasks) tasks.replaceChildren();
+    //projects.forEach((proje))
+}
+
+export function displayDefault() {
+    const checkElement = async selector => {
+        while ( document.querySelector(selector) === null) {
+            await new Promise( resolve => requestAnimationFrame(resolve) )
+        }
+        return document.querySelector(selector);
+    }
+
+    projects.forEach((project) => {
+        if (project.isDefault) {
+            checkElement(".project-name").then((selector) => {
+                selector.textContent = "hi";
+            });
+        } 
+    });
+}
+
+export function updateScreen() {
+    updateProjects();
+    updateTasks();
+}
+
+
