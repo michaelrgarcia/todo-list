c5262d3208956446a8e02052f77be1b6e1f484d0
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-12-10 11:43:55 -0800

    Add task and project creators

--- src/craftingTable.js
+++ src/craftingTable.js
@@ -1,12 +1,77 @@
-import iconModule from "./iconCreator";
+import icon1 from "./svgs/format-list-bulleted-square.svg";
+import icon8 from "./svgs/message-text-outline.svg";
+import icon9 from "./svgs/star-outline.svg";
+import icon10 from "./svgs/cog-outline.svg";
+
+function iconCreator(path) {
+    let svg = document.createElement("img");
+    svg.src = path;
+    svg.classList.add("svg");
+
+    return svg;
+}
+
+function domProjectCreator(title) {
+    const projectsMenu = document.getElementById("projects-menu");
+    let project = document.createElement("li");
+
+    let projectTitle = document.createElement("p");
+    projectTitle.textContent = title;
+
+    let projectIcon = iconCreator(icon1);
+
+    project.append(projectIcon, projectTitle);
+    projectsMenu.append(project);
+}
+
+function domTaskCreator(title) {
+    const taskMenu = document.getElementById("tasks");
+    let task = document.createElement("li");
+
+    let taskInfo = document.createElement("div");
+    taskInfo.classList.add("task");
+
+    let checkbox = document.createElement("div");
+    checkbox.classList.add("task-check");
+
+    let taskTitle = document.createElement("p");
+    taskTitle.textContent = title;
+
+    taskInfo.append(checkbox, taskTitle);
+
+    let taskSettings = document.createElement("div");
+    taskSettings.classList.add("task-config");
+
+    let notes = iconCreator(icon8);
+    let star = iconCreator(icon9);
+    let other = iconCreator(icon10);
+
+    taskSettings.append(notes, star, other);
+
+    task.append(taskInfo, taskSettings);
+    taskMenu.append(task);
+
+    //need to store description, dueDate, starred, notes somewhere
+    //maybe in its own dialog form
+}
 
 function elementCrafter(param1, param2, param3) {
     const icon = function(param1) {
-        let craftedIcon = iconModule.create(param1);
+        let craftedIcon = iconCreator(param1);
         return craftedIcon;
     }
 
-    return { param1, icon }
+    const domProject = function(param1) {
+        let craftedProject = domProjectCreator(param1);
+        return craftedProject;
+    }
+
+    const domTask = function(param1) {
+        let craftedTask = domTaskCreator(param1);
+        return craftedTask;
+    }
+
+    return { param1, icon, domProject, domTask };
 }
 
 export default elementCrafter();
