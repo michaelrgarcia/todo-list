5cbc1b0a42620b65b2d34312e883f3ccebd4cb7d
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-12-21 00:09:51 -0800

    Add dialog close button functionality

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import { displayDefault, updateScreen } from "./projectTaskRender.js";
 const content = document.getElementById("content");
 content.append(header(), main(), dialog());
 
+const dialogSelector = document.querySelector("dialog");
+
 displayDefault();
 
 window.addEventListener("click", function(event) {
     if (event.target.id == "add-task") {
-        document.querySelector("dialog").showModal();
+        dialogSelector.showModal();
         addTaskPrompt();
+    }
+    if (event.target.className === "svg close-dialog") {
+        dialogSelector.close();
     }
 }); 
 
