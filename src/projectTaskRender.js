fb3326006c746c7a73f34131bc13bb4ee89bf30a
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-12-22 11:55:49 -0800

    Create dom values -> task function

--- src/projectTaskRender.js
+++ src/projectTaskRender.js
@@ -29,6 +29,21 @@ function updateTasks() {
     });
 }
 
+export function domCreateTask() {
+    const title = document.getElementById("task-title");
+    const details = document.getElementById("task-details");
+    const due = document.getElementById("task-due");
+    
+    const dialogForm = document.querySelector("dialog");
+
+    if (title.value !== "") {
+        const newTask = createTask(title.value, details.value, "", due.value, false);
+        test.tasks.push(newTask);
+        dialogForm.close();
+        updateScreen();
+    }
+}
+
 export function displayDefault() {
     const checkElement = async selector => {
         while ( document.querySelector(selector) === null) {
