e126dbae0f42d343b5ebfaf1c94b9352d583afbf
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-12-22 10:48:28 -0800

    Add selected property for adding tasks

--- src/projectTaskCreator.js
+++ src/projectTaskCreator.js
@@ -1,7 +1,7 @@
-export function createProject(title, isDefault) {
+export function createProject(title, isDefault, selected) {
     let tasks = [];
 
-    return { title, isDefault, tasks };
+    return { title, isDefault, selected, tasks };
 }
 
 export function createTask(title, description, notes, dueDate, starred) {
