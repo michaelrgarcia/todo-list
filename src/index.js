c336a366b7be7d0b1da7c0d7c6bf7a7fb2f64146
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-12-24 22:08:16 -0800

    Pass project data attribute for array reference

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
-import { header, main, dialog, addTaskPrompt } from "./siteBuilder.js";
-import { displayDefault, updateScreen } from "./projectTaskRender.js";
-import { domCreateTask } from "./domLogic.js";
+import { header, main, dialog, addTaskPrompt, addProjectPrompt } from "./siteBuilder.js";
+import { displayDefault, updateProjects, updateTasks, domCreateTask, selectProject } from "./projectTaskRender.js";
 
 const content = document.getElementById("content");
 content.append(header(), main(), dialog());
 
-const dialogSelector = document.querySelector("dialog");
-
-
-
 displayDefault();
 
 window.addEventListener("click", function(event) {
-    if (event.target.id == "add-task") {
+    const dialogSelector = document.querySelector("dialog");
+
+    if (event.target.dataset.pnum) {
+        selectProject(event.target.dataset.pnum);
+    }
+    if (event.target.id === "add-task") {
         dialogSelector.showModal();
         addTaskPrompt();
-    }
-    if (event.target.className === "svg close-dialog") {
-        dialogSelector.close();
     }
     if (event.target.className === "submit-task") {
         domCreateTask();
     }
+    if (event.target.id === "add-project") {
+        dialogSelector.showModal();
+        addProjectPrompt();
+    }
+    if (event.target.className === "submit-project") {
+
+    }
+    if (event.target.className === "svg close-dialog") {
+        dialogSelector.close();
+    }
 }); 
 
-updateScreen();
+updateProjects();
