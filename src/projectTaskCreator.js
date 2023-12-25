9539c866cf6bf252b5be80374e7a9a9b76533fec
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2023-12-24 21:18:22 -0800

    Rename to domLogic

--- src/projectTaskCreator.js
+++
@@ -1,9 +0,0 @@
-export function createProject(title, isDefault, selected) {
-    let tasks = [];
-
-    return { title, isDefault, selected, tasks };
-}
-
-export function createTask(title, description, notes, dueDate, starred) {
-    return { title, description, notes, dueDate, starred };
-}
