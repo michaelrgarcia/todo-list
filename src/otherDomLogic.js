c85ee5c8326362c409c6d9214c4661c83b789f80
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-01-06 12:00:03 -0800

    Move submit + dialog reliant functions here

---
+++ src/otherDomLogic.js
@@ -0,0 +1,75 @@
+import { getTask, updateTasks } from "./projectTaskRender";
+import { getProjects } from "./siteLogic";
+
+
+export function getTaskFromDialog() {
+    const dialogForm = document.querySelector("dialog");
+
+    const taskNum = dialogForm.dataset.tnum;
+    const task = getTask(taskNum);
+
+    return task;
+}
+
+export function changeDialogTaskNum(taskNum) {
+    const dialogForm = document.querySelector("dialog");
+
+    dialogForm.setAttribute("data-tnum", taskNum);
+}
+
+export function displayTaskTitle() {
+    const domTaskTitle = document.getElementById("new-task-title");
+
+    const task = getTaskFromDialog();
+
+    domTaskTitle.value = task.title;
+}
+
+export function submitDetails() {
+    const dialogForm = document.querySelector("dialog");
+    const domTaskDetails = document.getElementById("task-details");
+
+    const task = getTaskFromDialog();
+
+    if (!task.completed) {
+        if (task.details !== domTaskDetails.value) {
+            let confirmation = confirm("Confirm Changes");
+    
+            if (confirmation === true) {
+                task.details = domTaskDetails.value;
+    
+                dialogForm.close();
+            }
+        } else {
+            dialogForm.close();
+        }
+    }
+}
+
+export function submitTaskTitle() {
+    const dialogForm = document.querySelector("dialog");
+    const domTaskTitle = document.getElementById("new-task-title");
+
+    const task = getTaskFromDialog();
+
+    const projects = getProjects();
+
+    const selectedProject = projects.find((project) => project.selected === true);
+    
+    if (task.title !== domTaskTitle.value) {
+        let confirmation = confirm("Confirm Changes");
+    
+        if (confirmation === true) {
+            task.title = domTaskTitle.value;
+    
+            dialogForm.close();
+            updateTasks(selectedProject);
+        } else {
+            dialogForm.close();
+        }
+    }
+}   
+
+export function showProjectOptions(project) {
+    
+}
